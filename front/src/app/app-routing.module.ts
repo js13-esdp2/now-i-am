922fd6905d66a268f9dee6f9135929392cbdf80d
Alina <alina.agibetova@gmail.com>
2022-04-28 01:25:43 +0600

    #18 Сделала главную страницу с опубликованием занятия, если пользователь не зарегистрирован, при зарегистрированном пользователе на главной странице отобразила создание занятия. Изменила роуты для отображения главной страницы. Добавила в компоненте layout.component.html ссылку на зарегистрированного и не зарегистрированного пользователя.

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,18 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
-import { AuthGuardService } from './services/auth-guard.service';
 import { SearchComponent } from './pages/search/search.component';
 import { NewPostComponent } from './pages/new-post/new-post.component';
 import { PostsComponent } from './pages/posts/posts.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
-import { RoleGuardService } from './services/role-guard.service';
 import { StatisticComponent } from './pages/statistic/statistic.component';
 
 const routes: Routes = [
-  {path: 'www', component: HomeComponent, canActivate: [AuthGuardService]},
-  {path: '', canActivate: [AuthGuardService], children: [
+  {path: 'www', component: HomeComponent},
+  {path: '', children: [
       {path: '', component: HomeComponent},
       {path: 'profile', children: [
           {path: '', component: ProfileComponent},
--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('f') form!: NgForm;
+  isLoading = false;
+  isSearched = false;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    this.isLoading = true;
+    this.isSearched = true;
+    const searchTitle = (this.form.value).search;
+    void this.router.navigate(['/statistic'], {queryParams: {title: searchTitle}})
+  }
 }
