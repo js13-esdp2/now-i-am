6770feab8426562ab4d48574352b6325c7c1b217
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-04-29 00:10:23 +0600

    #23 Настроила тестовое окружение. Добавила тесты для регистрации и логина.

--- api/config.js
+++ api/config.js
@@ -2,20 +2,35 @@ const path = require('path');
 
 const rootPath = __dirname;
 
+let dbUrl = 'mongodb://localhost/now-i-am';
+let port = 8000;
+
+if (process.env.NODE_ENV === 'test') {
+    dbUrl = 'mongodb://localhost/now-i-am-test';
+    port = 8010;
+}
+
+
 module.exports = {
-  rootPath,
-  uploadPath: path.join(rootPath, 'public/uploads'),
-  avatarAllowedTypes: ['.png', '.gif', '.jpg', '.jpeg'],
-  mongo: {
-    db: 'mongodb://localhost/now-i-am',
-    options: {useNewUrlParser: true},
-  },
+    corsWhiteList: [
+        'http://localhost:4200',
+        'https://localhost:4200',
+        'http://localhost:4210'
+    ],
+    port,
+    rootPath,
+    uploadPath: path.join(rootPath, 'public/uploads'),
+    avatarAllowedTypes: ['.png', '.gif', '.jpg', '.jpeg'],
+    mongo: {
+        db: dbUrl,
+        options: {useNewUrlParser: true},
+    },
     facebook: {
-    appId: '1036035837346520',
-      appSecret: '4349f330d6caa54978bfcd218e43464d'
+        appId: '1036035837346520',
+        appSecret: '4349f330d6caa54978bfcd218e43464d'
     },
     google: {
-      appId: '40811209122-h2vmbta5hlsirf6rkjtahnuiv4rdko4u.apps.googleusercontent.com',
+        appId: '40811209122-h2vmbta5hlsirf6rkjtahnuiv4rdko4u.apps.googleusercontent.com',
         appSecret: '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'
     }
 };
