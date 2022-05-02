3d3a0594821b7cc330e04500312ced5637abbad3
Roman Gorbachev <rn.gorbachev@gamil.com>
2022-05-02 23:11:30 +0600

    #19 Дабавил тест на проверку отображения страницы со статистикой

--- tests/step_definitions/steps.js
+++ tests/step_definitions/steps.js
@@ -7,6 +7,8 @@ Given('я нахожусь на странице {string}', (page) => {
             return I.amOnPage('/register');
         case "Логин":
             return I.amOnPage('/login');
+        case "Статистика":
+            return I.amOnPage('/statistic');
         default:
             return I.amOnPage('/');
     }
@@ -27,4 +29,21 @@ Given('нажимаю на кнопку на кнопку формы {string}', 
 Given('я должен увидеть текст {string}', (text) => {
     I.wait(2);
     I.see(text);
+});
+
+Given('я ввожу в поле формы:', (table) => {
+    table.rows.forEach((row) => {
+        row.cells[0];
+        row.cells[1];
+        I.fillField(row.cells[0].value, row.cells[1].value);
+    })
+});
+
+Given('нажимаю на кнопку на кнопку формы {string}', (buttonText) => {
+    I.click(buttonText, {css: 'form'});
+});
+
+Given('я должен увидеть текст {string}' , (text) => {
+    I.wait(2);
+    I.see(text, {css: '.amount-user__title'});
 });
