c0503c5bd2c2b28e6469ce386c2c86307d71e0a5
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-05-27 23:40:25 +0600

    #46 Изменила конфигурацию на фронте

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   apiUrl: 'http://159.223.23.60:8000',
   apiUrlCountries: 'https://restcountries.com/v3.1',
   webSocketApiUrl: 'ws://159.223.23.60:8000/chat-ws',
+  webSocketUrl: 'ws://159.223.23.60:8000',
   facebookAppId: '1036035837346520',
   googleAppId: '40811209122-h2vmbta5hlsirf6rkjtahnuiv4rdko4u.apps.googleusercontent.com',
   vkAppId: '8157037'
