eb577dd395b9ab05457696f2eab1c04ee5b2aac4
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-05-28 03:08:59 +0600

    #46 Были изменены роут-пути для WS

--- front/src/environments/environment.bdd.ts
+++ front/src/environments/environment.bdd.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiUrl: 'http://localhost:8010',
   apiUrlCountries: 'https://restcountries.com/v3.1',
   webSocketApiUrl: 'ws://localhost:8010/chat-ws',
-  webSocketUrl: 'ws://localhost:8000',
+  webSocketUrl: 'ws://localhost:8010',
   facebookAppId: '1036035837346520',
   googleAppId: '40811209122-h2vmbta5hlsirf6rkjtahnuiv4rdko4u.apps.googleusercontent.com',
   vkAppId: '8157037'
--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   apiUrl: 'https://now-i-am.freeddns.org/api',
   apiUrlCountries: 'https://restcountries.com/v3.1',
-  webSocketApiUrl: 'ws://159.223.23.60:8000/chat-ws',
-  webSocketUrl: 'ws://159.223.23.60:8000',
+  webSocketApiUrl: 'wss://159.223.23.60:8000/chat-ws',
+  webSocketUrl: 'wss://now-i-am.freeddns.org/api',
   facebookAppId: '1036035837346520',
   googleAppId: '40811209122-h2vmbta5hlsirf6rkjtahnuiv4rdko4u.apps.googleusercontent.com',
   vkAppId: '8157037'
