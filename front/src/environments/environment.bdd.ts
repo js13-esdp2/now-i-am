d70764885579e72c1275be17bb0c274460208e30
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-06-13 14:01:00 +0600

    #46 Исправлены не проходящие проверку тесты

--- front/src/environments/environment.bdd.ts
+++ front/src/environments/environment.bdd.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8010',
   apiUrlCountries: 'https://restcountries.com/v3.1',
-  webSocketApiUrl: 'ws://localhost:8010/chat-ws',
   webSocketUrl: 'ws://localhost:8010',
   facebookAppId: '1036035837346520',
   googleAppId: '40811209122-h2vmbta5hlsirf6rkjtahnuiv4rdko4u.apps.googleusercontent.com',
--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://now-i-am.freeddns.org/api',
   apiUrlCountries: 'https://restcountries.com/v3.1',
-  webSocketApiUrl: 'wss://159.223.23.60:8000/chat-ws',
   webSocketUrl: 'wss://159.223.23.60',
   facebookAppId: '1036035837346520',
   googleAppId: '40811209122-h2vmbta5hlsirf6rkjtahnuiv4rdko4u.apps.googleusercontent.com',
