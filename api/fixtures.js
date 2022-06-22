4f4e5410ed7467697ec7a82b9dd799bb2e64d2bd
Roman Gorbachev <rn.gorbachev@gamil.com>
2022-06-22 16:55:37 +0600

    #73 Исправил баг (создал дополнительный guard)

--- api/fixtures.js
+++ api/fixtures.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const config = require('./config');
-
 const User = require('./models/User');
 const Post = require('./models/Post');
 const ChatRoom = require('./models/ChatRoom');
